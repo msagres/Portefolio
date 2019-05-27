d6b2702fcc6bd441cf6767b756b22c66ebebb24a
Miguel Costa <ambcslb1@gmail.com>
2019-05-27 23:40:03 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
         $('.top-nav').toggleClass('open');
     });
 
-    $('.top-nav nav-link').on('click', function () {
-        $('menutoggler').removeClass('open');
-        $('.tap-nav').removeClass('open');
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
     });
 
     $('nav a[href*="#"]').on('click', function () {
